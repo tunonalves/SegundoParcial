743a37c84eaba202f2ca83ff3b6ac5c61a924d18
Fedeico <tunonalves@gmail.com>
2021-11-15 00:39:36 -0300

    var

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StudentListComponent } from './components/student-list/student-list.com
 import { ScoreListComponent } from './components/score-list/score-list.component';
 import { ScoreAddComponent } from './components/score-add/score-add.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule  
+    ReactiveFormsModule,
+    HttpClientModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/student-list/student-list.component.ts
+++ src/app/components/student-list/student-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Students } from 'src/app/models/students';
 import { CareerService } from 'src/app/services/career.service';
 import { StudentsService } from 'src/app/services/students.service';
+import { Career } from 'src/app/models/careers';
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
@@ -10,14 +11,22 @@ import { StudentsService } from 'src/app/services/students.service';
 export class StudentListComponent implements OnInit {
   notfound =  false;
   arrayStudent: Students[] = [];
+  arraycareer: Career[] = [];
+  idcarrer: number = 0;
 
   constructor(private servicestudent: StudentsService, private servicecareer: CareerService) { }
 
   ngOnInit(): void {
   }
+
   getstudent(){
     this.notfound = false;
     this.servicestudent.getStudent().subscribe((studentFromTheApi: Students) => {
+      this.idcarrer = studentFromTheApi.carrerId;
+      this.servicecareer.getcareer(this.idcarrer).subscribe((careerfromapi: Career)=>{
+        this.arraycareer.push(careerfromapi);
+      },(err2: any)=>{console.error(err2);}
+      );
       this.arrayStudent.push(studentFromTheApi);
     },(err: any) =>{
       console.error(err);
